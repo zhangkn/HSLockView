7ce1a25a38ca66bba4ee3c2b270a099dc5d74198
Kevin zhang <929118967@qq.com>
2016-04-21 09:00:03 +0800

    Initial Commit

---
+++ 20140621-手势解锁Tests/_0140621_____Tests.m
@@ -0,0 +1,39 @@
+//
+//  _0140621_____Tests.m
+//  20140621-手势解锁Tests
+//
+//  Created by devzkn on 4/21/16.
+//  Copyright © 2016 hisun. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0140621_____Tests : XCTestCase
+
+@end
+
+@implementation _0140621_____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
